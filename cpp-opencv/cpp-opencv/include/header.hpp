095baf4fea0f7d77576f837da2c437c83069248e
Yevgeniy Simonov <simonov.yevgeniy@gmail.com>
2022-07-07 23:45:02 +0800

    updated header file

--- cpp-opencv/cpp-opencv/include/header.hpp
+++ cpp-opencv/cpp-opencv/include/header.hpp
@@ -1,8 +1,7 @@
-﻿// cpp-opencv.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#ifndef CPPOPENCV_HPP
+#define CPPOPENCV_HPP
 
 #include <iostream>
 
-// TODO: Reference additional headers your program requires here.
+#endif
+
